b0c1f1a96bac8f4498c54a0ef634073e6bc91289
Pablo Martin Clemente <martin.clemente@cencosud.com>
2022-09-27 10:07:22 -0300

    Cambio de imagen al clickear miniatura indicando cual se encuentra seleccionada

--- public/js/product.js
+++ public/js/product.js
@@ -1,14 +1,23 @@
 window.addEventListener("load", function () {
-    let miniaturas = document.querySelectorAll('.main-product__miniaturas img')
-    let imagenPrincipal = document.querySelector('.imagenPrincipal')
-    console.log(miniaturas)
-    for (let i = 0; i < miniaturas.length; i++) {
-        miniaturas[i].classList.remove('img-selected')
-    }
-    for (let i = 0; i < miniaturas.length; i++) {
-         miniaturas[i].addEventListener ('click', function (e) {
-       imagenPrincipal.src = miniaturas[i].src
-       this.classList.add('img-selected')
-        })
-    }
+  let miniaturas = document.querySelectorAll('.main-product__miniaturas img')
+  let imagenPrincipal = document.querySelector('.imagenPrincipal')
+
+  
+  
+  for (let i = 0; i < miniaturas.length; i++) {
+    miniaturas[i].addEventListener ('click', function (e) {
+      
+    removeSelected(miniaturas);
+
+    imagenPrincipal.classList.add('fadeIn');
+    imagenPrincipal.src = miniaturas[i].src
+    this.classList.add('img-selected')
+  })
+  }
+});
+
+function removeSelected(miniaturas){
+  miniaturas.forEach(img => {
+    img.classList.remove('img-selected');
   });
+}
